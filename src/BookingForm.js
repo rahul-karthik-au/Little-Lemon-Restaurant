b0274781fc6fdc9d514aaef2affa75dce44de6e3
Rahul Karthik <rahulkarthik@rahuls-air.lan>
2024-04-06 13:50:20 -0500

    Bug Fixes

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -4,7 +4,11 @@ function BookingForm(props){
     const [date, setDate] = useState(""); 
     const [time, setTime] = useState(""); 
     const [guests, setGuests] = useState("");
-    const [occasion,setOccasion] = useState("");
+    const [occasion,setOccasion] = useState("--select occasion--");
+
+    function getIsFormValid(){
+        return (date && time && (guests>0 && guests<11) && occasion !== "--select occasion--");
+    }
 
     function clearForm(){
         setDate("");
@@ -28,32 +32,33 @@ function BookingForm(props){
             <input type="date" id="res-date" value={date} onChange={(e) => { 
                setDate(e.target.value);
                props.dispatch({payload:{date:e.target.value}}) 
-             }}></input>
+             }} required></input>
 
             <label htmlFor="res-time">Choose time</label>
             <select id="res-time" value={time} onChange={(e) => { 
                setTime(e.target.value); 
-             }}>
+             }} required>
                 {props.timings.map((t)=>{return <option key={t}>{t}</option>})}
             </select>
 
             <label htmlFor="guests">Number of guests</label>
             <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => { 
                setGuests(e.target.value); 
-             }}></input>
+             }} required></input>
 
             <label htmlFor="occasion">Occasion</label>
             <select id="occasion" value={occasion} onChange={(e) => { 
                setOccasion(e.target.value); 
-             }}>
+             }} required>
+                <option>--select occasion--</option>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
 
-            <input className="btn" type="submit" value="Make Your reservation" onClick={handleSubmit}></input>
+            <button className="btn" disabled={!getIsFormValid()} onClick={handleSubmit} aria-label="On Click">Make Your Reservation</button>
         </form>
         </>
     ) 
 }
 
-export default BookingForm;
+export default BookingForm; 
--- src/ConfirmedBooking.js
+++ src/ConfirmedBooking.js
@@ -1,7 +1,20 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+
 
 function ConfirmedBooking(){
+    const navigate = useNavigate();
+    useEffect(()=>{
+        setTimeout(function(){
+            navigate("/")
+        }, 1500);
+    })
+
+    
+    
     return(
         <>
         <Header />
